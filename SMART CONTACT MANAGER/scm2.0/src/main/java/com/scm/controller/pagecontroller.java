f1c5df07da53a3a0268e73dd833c2d950c87697f
AryamanDhoundiyal <143902685+AryamanDhoundiyal@users.noreply.github.com>
2024-05-14 23:33:09 +0530

    Add files via upload

---
+++ SMART CONTACT MANAGER/scm2.0/src/main/java/com/scm/controller/pagecontroller.java
@@ -0,0 +1,12 @@
+package com.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class pagecontroller {
+    @RequestMapping("/home")
+     public String home() {
+        System.out.println("Home page handler");
+     return "home";
+    }
+}
